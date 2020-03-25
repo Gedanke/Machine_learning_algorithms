da286c114f59fb0a75a0243da4381b8241d55809
Gedanke <47662202+Gedanke@users.noreply.github.com>
2020-03-25 16:15:23 +0800

    重写了绘制树的方法
    重写了绘制树的方法

--- 代码/DecisionTree/treePlotter/__init__.py
+++ 代码/DecisionTree/treePlotter/__init__.py
@@ -1,87 +1,120 @@
-# _*_ coding: UTF-8 _*_
+# -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
 
+"""
+绘决策树的函数
+"""
 
-"""绘决策树的函数"""
-decisionNode = dict(boxstyle="sawtooth", fc="0.8")  # 定义分支点的样式
-leafNode = dict(boxstyle="round4", fc="0.8")  # 定义叶节点的样式
-arrow_args = dict(arrowstyle="<-")  # 定义箭头标识样式
+'''定义分支点的样式'''
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+'''定义叶节点的样式'''
+leafNode = dict(boxstyle="round4", fc="0.8")
+'''定义箭头标识样式'''
+arrow_args = dict(arrowstyle="<-")
 
 
-# 计算树的叶子节点数量
-def getNumLeafs(myTree):
-    numLeafs = 0
-    firstStr = list(myTree.keys())[0]
-    secondDict = myTree[firstStr]
-    for key in secondDict.keys():
-        if type(secondDict[key]).__name__ == 'dict':
-            numLeafs += getNumLeafs(secondDict[key])
+def get_num_leafs(tree):
+    """
+    计算树的叶子节点数量
+    :param tree: 树
+    :return: 叶子节点数
+    """
+    num_leafs = 0
+    first_str = list(tree.keys())[0]
+    second_dict = tree[first_str]
+    for key in second_dict.keys():
+        if type(second_dict[key]).__name__ == "dict":
+            num_leafs += get_num_leafs(second_dict[key])
         else:
-            numLeafs += 1
-    return numLeafs
+            num_leafs += 1
+    return num_leafs
 
 
-# 计算树的最大深度
-def getTreeDepth(myTree):
-    maxDepth = 0
-    firstStr = list(myTree.keys())[0]
-    secondDict = myTree[firstStr]
-    for key in secondDict.keys():
-        if type(secondDict[key]).__name__ == 'dict':
-            thisDepth = 1 + getTreeDepth(secondDict[key])
+def get_depth(tree):
+    """
+    计算树的最大深度
+    :param tree: 树
+    :return: 树的最大深度
+    """
+    max_depth = 0
+    first_str = list(tree.keys())[0]
+    second_dict = tree[first_str]
+    for key in second_dict.keys():
+        if type(second_dict[key]).__name__ == "dict":
+            this_depth = 1 + get_depth(second_dict[key])
         else:
-            thisDepth = 1
-        if thisDepth > maxDepth:
-            maxDepth = thisDepth
-    return maxDepth
+            this_depth = 1
+        if this_depth > max_depth:
+            max_depth = this_depth
+    return max_depth
 
 
-# 画出节点
-def plotNode(nodeTxt, centerPt, parentPt, nodeType):
-    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', \
-                            xytext=centerPt, textcoords='axes fraction', va="center", ha="center", \
-                            bbox=nodeType, arrowprops=arrow_args)
+def plot_node(node_txt, center_pt, parent_pt, node_type):
+    """
+    画出节点
+    :param node_txt:
+    :param center_pt:
+    :param parent_pt:
+    :param node_type:
+    :return:
+    """
+    create_plot.ax1.annotate(node_txt, xy=parent_pt, xycoords='axes fraction', xytext=center_pt,
+                             textcoords='axes fraction', va="center", ha="center", bbox=node_type,
+                             arrowprops=arrow_args)
 
 
-# 标箭头上的文字
-def plotMidText(cntrPt, parentPt, txtString):
-    lens = len(txtString)
-    xMid = (parentPt[0] + cntrPt[0]) / 2.0 - lens * 0.002
-    yMid = (parentPt[1] + cntrPt[1]) / 2.0
-    createPlot.ax1.text(xMid, yMid, txtString)
+def plot_mid_text(center_pt, parent_pt, txt_string):
+    """
+    标箭头上的文字
+    :param center_pt:
+    :param parent_pt:
+    :param txt_string:
+    :return:
+    """
+    lens = len(txt_string)
+    x_mid = (parent_pt[0] + center_pt[0]) / 2.0 - lens * 0.002
+    y_mid = (parent_pt[1] + center_pt[1]) / 2.0
+    create_plot.ax1.text(x_mid, y_mid, txt_string)
 
 
-def plotTree(myTree, parentPt, nodeTxt):
-    numLeafs = getNumLeafs(myTree)
-    depth = getTreeDepth(myTree)
-    firstStr = list(myTree.keys())[0]
-    cntrPt = (plotTree.x0ff + \
-              (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.y0ff)
-    plotMidText(cntrPt, parentPt, nodeTxt)
-    plotNode(firstStr, cntrPt, parentPt, decisionNode)
-    secondDict = myTree[firstStr]
-    plotTree.y0ff = plotTree.y0ff - 1.0 / plotTree.totalD
-    for key in secondDict.keys():
-        if type(secondDict[key]).__name__ == 'dict':
-            plotTree(secondDict[key], cntrPt, str(key))
+def plot_tree(tree, parent_pt, node_txt):
+    """
+    :param tree:
+    :param parent_pt:
+    :param node_txt:
+    :return:
+    """
+    num_leafs = get_num_leafs(tree)
+    depth = get_depth(tree)
+    first_str = list(tree.keys())[0]
+    center_pt = (plot_tree.x0ff + (1.0 + float(num_leafs)) / 2.0 / plot_tree.totalW, plot_tree.y0ff)
+    plot_mid_text(center_pt, parent_pt, node_txt)
+    plot_node(first_str, center_pt, parent_pt, decisionNode)
+    second_dict = tree[first_str]
+    plot_tree.y0ff = plot_tree.y0ff - 1.0 / plot_tree.totalD
+    for key in second_dict.keys():
+        if type(second_dict[key]).__name__ == 'dict':
+            plot_tree(second_dict[key], center_pt, str(key))
         else:
-            plotTree.x0ff = plotTree.x0ff + 1.0 / plotTree.totalW
-            plotNode(secondDict[key], \
-                     (plotTree.x0ff, plotTree.y0ff), cntrPt, leafNode)
-            plotMidText((plotTree.x0ff, plotTree.y0ff) \
-                        , cntrPt, str(key))
-    plotTree.y0ff = plotTree.y0ff + 1.0 / plotTree.totalD
+            plot_tree.x0ff = plot_tree.x0ff + 1.0 / plot_tree.totalW
+            plot_node(second_dict[key], (plot_tree.x0ff, plot_tree.y0ff), center_pt, leafNode)
+            plot_mid_text((plot_tree.x0ff, plot_tree.y0ff), center_pt, str(key))
+    plot_tree.y0ff = plot_tree.y0ff + 1.0 / plot_tree.totalD
 
 
-def createPlot(inTree):
+def create_plot(tree):
+    """
+    :param tree:
+    :return:
+    """
     fig = plt.figure(1, facecolor='white')
     fig.clf()
-    axprops = dict(xticks=[], yticks=[])
-    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
-    plotTree.totalW = float(getNumLeafs(inTree))
-    plotTree.totalD = float(getTreeDepth(inTree))
-    plotTree.x0ff = -0.5 / plotTree.totalW
-    plotTree.y0ff = 1.0
-    plotTree(inTree, (0.5, 1.0), '')
+    apropos = dict(xticks=[], yticks=[])
+    create_plot.ax1 = plt.subplot(111, frameon=False, **apropos)
+    plot_tree.totalW = float(get_num_leafs(tree))
+    plot_tree.totalD = float(get_depth(tree))
+    plot_tree.x0ff = -0.5 / plot_tree.totalW
+    plot_tree.y0ff = 1.0
+    plot_tree(tree, (0.5, 1.0), '')
     plt.show()
